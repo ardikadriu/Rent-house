ec17d13687c6eb0fbf87636f1bc37b0f4392f675
Ardi Kadriu <ardikadriu156@gmail.com>
2024-01-10 16:24:44 +0100

    Added Links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 const Footer = () => {
   return (
     <div className=" flex items-center justify-center ">
       <div className="bg-white text-black w-[615px] rounded-t-[600px] h-[236px] absolute bottom-[-41px] flex flex-col  items-center">
         <h2 className="font-poppins mt-[43px]">Reservation Now</h2>
-        <button className="bg-[#04242F] text-white px-[41px] py-3 rounded-[26px] mt-[24px] text-[24px] font-bold font-poppins">
-          BOOK NOW
-        </button>
+        <Link href={"Newpage"}>
+          <button className="bg-[#04242F] text-white px-[41px] py-3 rounded-[26px] mt-[24px] text-[24px] font-bold font-poppins">
+            BOOK NOW
+          </button>
+        </Link>
       </div>
     </div>
   );
